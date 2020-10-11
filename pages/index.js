3722a98259962e507b39465ba930800effb50129
Marfy Carlohanda <marfy.naked@gmail.com>
2020-10-12 03:55:07 +0700

    lp hampir kelar

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <div>
-          <img style={{"border-radius":"50%", border:"2px solid #ccc"}} src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-19/s150x150/92373161_211333956788365_8151595336486354944_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_ohc=ShE93RhfmMEAX-ZsUKH&oh=3ac09e452b4466a2d415c098b22855b7&oe=5FAA1F26" alt=""/>
+          <img style={{borderRadius:"50%", border:"2px solid #ccc"}} src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-19/s150x150/92373161_211333956788365_8151595336486354944_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_ohc=ShE93RhfmMEAX-ZsUKH&oh=3ac09e452b4466a2d415c098b22855b7&oe=5FAA1F26" alt=""/>
         </div>
         <h1 className={styles.title}>
           Welcome to, {' '}
@@ -19,30 +19,31 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+        👋 Hi Sister & Ladies, Lagi cari tas 👜 & sepatu 👠 branded?! Kami menjual produk ex-counter dari brand terkenal Zara, Charles & Keith, Guess, Pedro, dll dengan harga mulai dari 300 ribuan 🤩💰 dan udah pasti original 💯%. Silahkan klik link yang ada di bawah ini 👇👇 untuk kepoin kita lebih lanjut. Happy Shopping!!🛒 
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h4>Our Latest Catalogue &rarr;</h4>
+        <a href="https://bit.ly/meishashoes" className={styles.card}>
+            <h4>💬 Pesan Cepat? Chat Kami 💬</h4>
+            <div className={styles.hands}></div>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h4>Chat us for Fast Order &rarr;</h4>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h4>Join us to be a Reseller &rarr;</h4>
+          <a href="https://www.instagram.com/meishashoes.id/" className={styles.card}>
+            <h4>😍 Produk terbaru 😍</h4>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://www.instagram.com/stories/highlights/17869167259957944/"
             className={styles.card}
           >
-            <h4>Feels free for Complaints &rarr;</h4>
+            <h4>❤️ Testimonial Kami ❤️</h4>
+          </a>
+
+          <a
+            href="https://bit.ly/meishashoes"
+            className={styles.card}
+          >
+            <h4>✔️ Gabung Dropship/Reseller ✔️</h4>
           </a>
         </div>
       </main>
