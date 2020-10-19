6383aacd79b61ca71b1073d1881b8f9471aaaf0c
Marfy Carlohanda <marfy.naked@gmail.com>
2020-10-19 16:54:09 +0700

    ga gtm

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,19 @@ export default class MyDocument extends Document {
               })();
           `,
             }}
-          />  
+          />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RQT77NM96" /> 
+          <script dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-6RQT77NM96');
+                      `,
+            }}
+          />
+
         </Head>
         <body>
           <Main />
