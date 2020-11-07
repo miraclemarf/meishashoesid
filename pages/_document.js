9cd100ac3d7f2f2cd1740483ea19baf31d0f5fd3
Marfy Carlohanda <marfy.naked@gmail.com>
2020-11-07 14:05:44 +0700

    fb pixel

--- pages/_document.js
+++ pages/_document.js
@@ -17,20 +17,37 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RQT77NM96" /> 
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RQT77NM96" />
           <script dangerouslySetInnerHTML={{
-              __html: `
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
               gtag('config', 'G-6RQT77NM96');
                       `,
-            }}
+          }}
           />
-
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1300555053629135');
+            fbq('track', 'PageView');
+            `,
+          }}
+          />
         </Head>
         <body>
+          <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1300555053629135&ev=PageView&noscript=1"
+          /></noscript>
           <Main />
           <NextScript />
         </body>
