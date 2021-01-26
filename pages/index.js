0a408db9e59d71e540faf98fc6319836612aa886
Marfy Carlohanda <marfy.naked@gmail.com>
2021-01-27 02:25:36 +0700

    promo payday

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
         </div>
         <div style={{ textAlign: "center" }}>
           <a onClick={() => { fbq('track', 'Contact'); }} href="https://bit.ly/meishashoes" target="_blank" rel="noopener noreferrer">
-            <img className={'promo'} src="/promo.gif" style={{ maxWidth: "40%" }} />
+            <img className={'promo'} src="/promo-pd-270121.jpg" style={{ maxWidth: "40%" }} />
           </a>
         </div>
         <div style={{ marginBottom: "1.5em", marginTop: "2.5em", textAlign: "center" }}>
